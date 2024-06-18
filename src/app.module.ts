65cd6bb64f2a3d2f55e3241b6b8c15b915e4fcaa
mrquangthai278 <mrquangthai278@gmail.com>
2024-06-18 23:10:30 +0700

    First commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,19 +9,16 @@ import { TagModule } from "./tag/tag.module";
 
 @Module({
   imports: [
-    TypeOrmModule
-      .forRoot
-      //   {
-      //   type: "postgres",
-      //   host: "localhost",
-      //   port: 5432,
-      //   username: "postgres",
-      //   password: "123456",
-      //   database: "nestjsrealworld",
-      //   entities: ["dist/**/*.entity.js"],
-      //   synchronize: true,
-      // }
-      (),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "123456",
+      database: "nestjsrealworld",
+      entities: ["dist/**/*.entity.js"],
+      synchronize: true,
+    }),
     ArticleModule,
     UserModule,
     ProfileModule,
